1ab25f09642f78f2dbaf91b515e80b38e4fd3b77
Hossain-sakib <mskib567@gmail.com>
2023-11-27 00:54:42 +0600

    created jwt realted api

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+
+// middleware
 app.use(cors());
 app.use(express.json());
+
+
 
 
 
@@ -25,6 +30,26 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const userCollection = client.db("zzDB").collection("user");
+
+
+    // jwt related api
+    app.post("/jwt", async (req, res) => {
+        const user = req.body;
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+          expiresIn: "2h",
+        });
+        res.send({ token });
+      });
+
+
+    
+    
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
