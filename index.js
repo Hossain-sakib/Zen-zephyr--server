9e1022f952970a82b5489518864ac1eec02f4d12
Hossain-sakib <mskib567@gmail.com>
2023-11-28 02:21:56 +0600

    created posts get api by specific id

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.y2vlbq8.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,6 +27,22 @@ async function run() {
     await client.connect();
     const userCollection = client.db("zzDB").collection("user");
     const postCollection = client.db("zzDB").collection("post");
+
+    // middlewares
+    const verifyToken = (req, res, next) => {
+        // console.log('inside verify token',req.headers.authorization);
+        if (!req.headers.authorization) {
+          return res.status(401).send({ message: "unauthorized access" });
+        }
+        const token = req.headers.authorization.split(" ")[1];
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+          if (err) {
+            return res.status(401).send({ message: "unauthorized  access" });
+          }
+          req.decoded = decoded;
+          next();
+        });
+      };
 
     // jwt related api
     app.post("/jwt", async (req, res) => {
@@ -53,6 +69,12 @@ async function run() {
       const result = await userCollection.find().toArray();
       res.send(result);
     });
+    app.get("/users/:email",async (req,res)=>{
+        const email = req.params.email;
+        const query = {email: email};
+        const result = await userCollection.findOne(query);
+        res.send(result);
+    })
 
     //   post related api
     app.post("/post", async (req, res) => {
@@ -64,6 +86,12 @@ async function run() {
       const result = await postCollection.find().toArray();
       res.send(result);
     });
+    app.get("/post/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id)};
+        const result = await postCollection.findOne(query);
+        res.send(result);
+      });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
